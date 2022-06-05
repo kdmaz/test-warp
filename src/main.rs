4a6943fd0dcde973a4982f496ae42905b623007f
kyle <kyle.mazeika@gmail.com>
2022-06-05 14:52:43 -0400

    adds questions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,57 @@
+use serde::Serialize;
+use std::{io::Error, io::ErrorKind, str::FromStr};
 use warp::Filter;
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_owned())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    Ok(warp::reply::json(&question))
+}
 
 #[tokio::main]
 async fn main() {
-    let filter = warp::path("hi").map(|| format!("hi!"));
-    warp::serve(filter).run(([127,0,0,1], 8080)).await;
+    let routes = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
